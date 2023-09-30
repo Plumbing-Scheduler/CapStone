7a52eb0993cb50190f43090f74279745416663f0
IsaacM1210 <1210Mhamed@gmail.com>
2023-09-30 13:57:37 -0600

    Added Update Work Order by ID

--- backend/routes/workOrdersRoute.js
+++ backend/routes/workOrdersRoute.js
@@ -31,10 +31,10 @@ router.post('/', async (request, response) => {
 //Route to Get ALL workOrders
 router.get('/', async (request, response) => {
     try {
-        const workOrders = await WorkOrder.find({});
+        const result = await WorkOrder.find({});
         return response.status(200).send({
-            count: workOrders.length,
-            data: workOrders
+            count: result.length,
+            data: result
         });
     } catch (error) {
         console.log(error);
@@ -47,8 +47,11 @@ router.get('/:id', async (request, response) => {
     try {
         const { id } = request.params;
 
-        const workOrder = await WorkOrder.findById(id)
-        return response.status(200).send(workOrder);
+        const result = await WorkOrder.findById(id)
+        if(!result){
+            return response.status(404).send({message: 'Work Order Not Found'});
+        }
+        return response.status(200).send(result);
     } catch (error) {
         console.log(error);
         response.status(500).send({message: error.message});
@@ -57,7 +60,22 @@ router.get('/:id', async (request, response) => {
 
 //Route to update workorder by ID
 router.put('/:id', async (request, response) => {
-    
+    try {
+        if(!request.body.s_description || !request.body.s_startDate || !request.body.s_cost || !request.body.customerID){
+            return response.status(400).send({message: 'All required fields must be filled'});
+        }
+
+        const { id } = request.params;
+
+        const result = await WorkOrder.findByIdAndUpdate(id, request.body);
+        if(!result){
+            return response.status(404).send({message: 'Work Order Not Found'});
+        }
+        return response.status(200).send({message: 'Work Order Updated!'})
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({message: error.message});
+    }
 });
 
 //Route to Delete worderOrder by ID
