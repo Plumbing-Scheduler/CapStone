004ca7a34bb9f2993204d98497c31e140c9fa07c
IsaacM1210 <1210Mhamed@gmail.com>
2023-09-30 11:08:18 -0600

    Modified Connection process

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-
 const PORT = process.env.port || 3500;
+
+
 app.use(express.json());
 
 //Default Route
@@ -13,16 +14,20 @@ app.get('/' ,(request, response) => {
     return response.status(234).send('Welcome to the beggining of the end!')
 });
 
+
+
 //Connection to MongoDB
 //Connection Process will need to be changed so than Mongo URL isnt in plain text!!!!!!
-//for Development Process
-try {
-    await mongoose.connect('mongodb+srv://Admin:hvHVFgTb1Ljxf4oC@plumbing-scheduler.rqelmif.mongodb.net/?retryWrites=true&w=majority')
-    console.log("connected to Mongo")
-} catch (error) {
-    console.log(error)
-}
+//for Development Process mongodb+srv://Admin:hvHVFgTb1Ljxf4oC@plumbing-scheduler.rqelmif.mongodb.net/?retryWrites=true&w=majority
+mongoose
+    .connect('mongodb+srv://Admin:hvHVFgTb1Ljxf4oC@plumbing-scheduler.rqelmif.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('App Connected to DataBase');
+        app.listen(PORT, () => {
+            console.log(`App is Listening on port ${PORT}`)
+        });
+    }
+    ).catch((error) => {
+        console.log(error);
+    });
 
-app.listen(PORT, () => {
-    console.log(`App listening on Port ${PORT}`);
-});
