a5221a9415587907f993a32894cb67521e31fc24
IsaacM1210 <1210Mhamed@gmail.com>
2023-09-30 11:42:40 -0600

    added Method Header Definitions

--- backend/routes/workOrdersRoute.js
+++ backend/routes/workOrdersRoute.js
@@ -1,20 +1,30 @@
-import express from "express";
+import express, { response } from "express";
 import workOrderModel from '../models/workOrderModel.js';
 
 const router = express.Router();
 
 //Route to add a new workOrder
-router.post();
+router.post('/', async (request, response) => {
+
+});
 
 //Route to Get ALL workOrders
-router.get();
+router.get('/', async (request, response) => {
+    
+});
 
 //Route to get workOrder by ID
-router.get();
+router.get('/:id', async (request, response) => {
+    
+});
 
 //Route to update workorder by ID
-router.put();
+router.put('/:id', async (request, response) => {
+    
+});
 
 //Route to Delete worderOrder by ID
-router.delete()
+router.delete('/:id', async (request, response) => {
+    
+})
 
