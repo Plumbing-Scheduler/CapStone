52872310f3dae5690a2e52c1b9333408425be332
IsaacM1210 <1210Mhamed@gmail.com>
2023-10-08 10:20:00 -0600

    Modified the style of Work Orders List table

--- frontend/src/pages/workOrders.jsx
+++ frontend/src/pages/workOrders.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AddNewButton from '../components/AddNewButton';
 import axios from 'axios';
 import Header from '../components/Header';
@@ -30,45 +30,45 @@ const WorkOrders = () => {
             </div>
             
 
-            <Paper sx={{ width: '70%', margin: 'auto'}}>
+            <Paper sx={{ width: '70%', margin: 'auto', border:"2px solid gray" , borderRadius: '5px', bgcolor: "#141414", color: "#d0d1d5",}}>
                 <table className='w-full text-xl'>
                     <thead>
-                        <tr>
-                            <th className='border-b-2 border-slate-600'>No.</th>
-                            <th className='border-b-2 border-slate-600'>Title</th>
-                            <th className='border-b-2 border-slate-600'>Cost</th>
-                            <th className='border-b-2 border-slate-600'>Emp</th>
-                            <th className='border-b-2 border-slate-600'>Customer</th>
-                            <th className='border-b-2 border-slate-600'>Operations</th>
+                        <tr className='border-b-4 border-slate-600 text-left pl-2'>
+                            <th className='pl-2'>No.</th>
+                            <th className='pl-2'>Title</th>
+                            <th className='pl-2'>Cost</th>
+                            <th className='pl-2'>Emp</th>
+                            <th className='pl-2'>Customer</th>
+                            <th className='pl-2'>Operations</th>
                         </tr>
                     </thead>
-                    <tbody className='h-4'>
+                    <tbody>
                         {workOrders.map((wo, index) => (
-                            <tr key={wo._id} className='h-8'>
-                                <td className='border border-slate-700 pl-2'>
+                            <tr key={wo._id} className='h-20 border-b border-slate-700'>
+                                <td className='pl-2'>
                                     {index + 1}
                                 </td>
-                                <td className='border border-slate-700 pl-2'>
+                                <td className='pl-2'>
                                     {wo.title}
                                 </td>
-                                <td className='border border-slate-700 pl-2'>
+                                <td className='pl-2'>
                                     {wo.s_cost}
                                 </td>
-                                <td className='border border-slate-700 pl-2'>
+                                <td className='pl-2'>
                                     {wo.assignedEmp}
                                 </td>
-                                <td className='border border-slate-700 pl-2'>
+                                <td className='pl-2'>
                                     {wo.customerID}
                                 </td>
-                                <td className='flex justify-evenly border border-slate-700'>
+                                <td className='flex justify-evenly items-center  h-20'>
 
                                     <Link to={`edit/${wo._id}`} className='link '>
                                         <EditIcon />
                                     </Link>
-                                    <Link to={`details/${wo._id}`} className='link '>
+                                    <Link to={`details/${wo._id}`} className='link'>
                                         <InfoOutlinedIcon />
                                     </Link>
-                                    <Link to={`delete/${wo._id}`} className='link '>
+                                    <Link to={`delete/${wo._id}`} className='link'>
                                         <DeleteOutlineIcon />
                                     </Link>
 
