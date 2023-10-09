58509ffca0ba8d2b9e15b6b54262674baebdad8a
IsaacM1210 <1210Mhamed@gmail.com>
2023-10-09 09:53:45 -0600

    Schedule Page

--- frontend/src/pages/Schedule.jsx
+++ frontend/src/pages/Schedule.jsx
@@ -14,22 +14,31 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import Spinner from 'react-bootstrap/Spinner';
 
 const Schedule = () => {
+    const [ loading, setLoading ] = useState(true)
     const currDate = Date.now();
-    const [ data, setData] = useState([{}]);
+    //const [ data, setData] = useState([{}]);
+   const data = [{ startDate: '2023-10-10T09:45', endDate: '2023-10-10T11:00', title: 'Meeting' },
+   { startDate: '2023-10-09T12:00', endDate: '2023-10-09T13:30', title: 'Go to a gym'  }]
 
     useEffect(() => {
         axios
             .get('http://localhost:3500/schedule')
             .then((response) => {
                 setData(response.data.data)
-                console.log(data)
+                setLoading(false)
+                //console.log(response.data.data)
             })
-    }, [])
+            .catch((error) => {
+                setLoading(false)
+                console.log(error)
+            })
+    }, []);
     return <Box m="20px">
         <Header title="SCHEDULE" subtitle="Calendar" />
+        {loading ? (<div className='w-5 m-auto h-5 pt-11 text-center'><Spinner /></div>) : (
         <Paper>
             <Scheduler data={data}>
             <ViewState defaultCurrentDate={currDate} defaultCurrentViewName="Week" />
@@ -43,7 +52,8 @@ const Schedule = () => {
             <DateNavigator />
             <ViewSwitcher />
             </Scheduler>
-            </Paper>
+        </Paper>
+        )}
     </Box>
 }
 
