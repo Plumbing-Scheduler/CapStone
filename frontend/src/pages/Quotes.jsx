7e21c2a7a11cd306d639df3c3410086b5d8f7512
Marcus Ponce <poncem51274@gmail.com>
2023-10-11 21:35:01 -0600

    added form for quote requests in the quote page

--- frontend/src/pages/Quotes.jsx
+++ frontend/src/pages/Quotes.jsx
@@ -1,10 +1,79 @@
-import { Box } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 import Header from "../components/Header";
+
 
 const Quotes = () => {
 
     return <Box m="20px">
         <Header title="QUOTE REQUESTS" subtitle="Select Quote" />
+        <Box
+                display="grid"
+                gap="30px"
+                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                sx={{
+                    gridColumn: "span 4",
+                    margin: "auto",
+                    width: '75%'
+                }} >
+                <TextField
+                    fullWidth
+                    type="number"
+                    variant='filled'
+                    label="Phone #"
+                    name="phone"
+                    id=""
+                    sx={{ gridColumn: "span 1" }}
+                />
+                <TextField
+                    fullWidth
+                    type="text"
+                    variant='filled'
+                    label="Email"
+                    name="email"
+                    id=""
+                    sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                    fullWidth
+                    multiline
+                    variant="filled"
+                    label="Description"
+                    required
+                    cols="30"
+                    rows="4"
+                    name="description"
+                    id=""
+                    sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                    fullWidth
+                    type="text"
+                    variant="filled"
+                    label="Buisness Name"
+                    name="businessname"
+                    id=""
+                    sx={{ gridColumn: "span 1" }}
+                />
+                <TextField
+                    fullWidth
+                    type="text"
+                    variant="filled"
+                    label="Address"
+                    name="address"
+                    id=""
+                    sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                    fullWidth
+                    type="number"
+                    variant='filled'
+                    label="Cost"
+                    name="cost"
+                    id=""
+                    sx={{ gridColumn: "span 1" }}
+                />
+
+            </Box>
 
     </Box>
 }
