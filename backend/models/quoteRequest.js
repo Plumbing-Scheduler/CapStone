0128577af01748f1f6bd800354344eea531a8f34
Samuel Huh <sam.huh56@gmail.com>
2023-10-14 00:04:00 -0600

    Created quoteRequest Controller and Model

---
+++ backend/models/quoteRequest.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+//Quote Request Database Model
+
+const quoteRequestSchema = mongoose.Schema(
+    {
+        phone: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        cost: {
+            type: Number,
+            required: true
+        },
+        busName: String,
+        email: String,
+    }
+);
+
+export const QuoteRequest = mongoose.model('QuoteRequest', quoteRequestSchema);
