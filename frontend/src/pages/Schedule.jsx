3672f36318af6ca65c3af51534537a4f1dfd1069
christian <christian.langcuyan@hotmail.com>
2023-10-23 00:47:05 -0600

    Made Schedule Responsive

--- frontend/src/pages/Schedule.jsx
+++ frontend/src/pages/Schedule.jsx
@@ -1,6 +1,11 @@
-import { Box } from "@mui/material";
-import Header from "../components/Header";
-import { Paper, useTheme } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Spinner from 'react-bootstrap/Spinner';
+import {
+    Box,
+    Paper,
+    useTheme,
+} from "@mui/material";
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
@@ -12,57 +17,55 @@ import {
     DateNavigator,
     ViewSwitcher
 } from '@devexpress/dx-react-scheduler-material-ui';
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Spinner from 'react-bootstrap/Spinner';
+import Header from "../components/Header";
 import { tokens } from "../theme";
 
 const Schedule = () => {
     const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const [loading, setLoading] = useState(true)
     const currDate = Date.now();
     const [data, setData] = useState([{}]);
-    //    const data = [{ startDate: '2023-10-10T09:45', endDate: '2023-10-10T11:00', title: 'Meeting' },
-    //    { startDate: '2023-10-09T12:00', endDate: '2023-10-09T13:30', title: 'Go to a gym'  }]
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios
-            .get('http://localhost:3500/schedule')
+        axios.get('http://localhost:3500/schedule')
             .then((response) => {
-                setData(response.data.data)
-                setLoading(false)
-                //console.log(response.data.data)
+                setData(response.data.data);
+                setLoading(false);
             })
             .catch((error) => {
-                setLoading(false)
-                console.log(error)
-            })
+                setLoading(false);
+                console.log(error);
+            });
     }, []);
-    return <div>
-        <Header title="SCHEDULE" subtitle="Calendar" />
-        <div className='text-center sm:max-2xl:flex justify-between p-2 m-3 shadow-lg' >
-            {loading ? (<div className='w-5 m-auto h-5 pt-11 text-center'><Spinner /></div>) : (
-                <Paper variant="h4">
-                    <Box flex="1 1 20%" >
-                        <Scheduler data={data} >
-                            <ViewState defaultCurrentDate={currDate} defaultCurrentViewName="Week" />
 
-                            <DayView startDayHour={6} endDayHour={18} />
-                            <WeekView startDayHour={6} endDayHour={18} />
-                            <MonthView />
+    // Determine if the screen width is smaller than a specific breakpoint
+    const isMobile = window.innerWidth <= 768; // You can adjust the breakpoint as needed
 
-                            <Appointments />
-                            <Toolbar />
-                            <DateNavigator />
-                            <ViewSwitcher />
-                        </Scheduler>
-                    </Box>
-                </Paper>
-            )}
-
+    return (
+        <div>
+            <Header title="SCHEDULE" subtitle="Calendar" />
+            <div className={`text-center sm:max-2xl:flex justify-between p-2 m-3 shadow-lg ${isMobile ? 'mobile-styles' : 'desktop-styles'}`}>
+                {loading ? (
+                    <div className='w-5 m-auto h-5 pt-11 text-center'><Spinner /></div>
+                ) : (
+                    <Paper variant="h4">
+                        <Box flex="1 1 20%">
+                            <Scheduler data={data}>
+                                <ViewState defaultCurrentDate={currDate} defaultCurrentViewName={isMobile ? "Day" : "Week"} />
+                                <DayView startDayHour={6} endDayHour={18} />
+                                <WeekView startDayHour={6} endDayHour={18} />
+                                <MonthView />
+                                <Appointments />
+                                <Toolbar />
+                                <DateNavigator />
+                                <ViewSwitcher />
+                            </Scheduler>
+                        </Box>
+                    </Paper>
+                )}
+            </div>
         </div>
-    </div>
-}
+    );
+};
 
 export default Schedule;
--- frontend/src/pages/workOrder/EditWorkOrder.jsx
+++ frontend/src/pages/workOrder/EditWorkOrder.jsx
@@ -203,3 +203,4 @@ export const CreateWorkOrder = () => {
 }
 
 export default CreateWorkOrder
+
