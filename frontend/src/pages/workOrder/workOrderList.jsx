2db627fe3217d07e9bf5b9e5b4ee7ed0e4d51362
IsaacM1210 <1210Mhamed@gmail.com>
2023-10-25 14:04:31 -0600

    removed old Work Order List

--- frontend/src/pages/workOrder/workOrderList.jsx
+++ frontend/src/pages/workOrder/workOrderList.jsx
@@ -135,55 +135,6 @@ export const WorkOrders = () => {
                         pageSizeOptions={[10, 25, 50, 100]}
 
                     />
-
-                    {/* <Paper sx={{ width: '70%', margin: 'auto', border: "2px solid gray", borderRadius: '5px', bgcolor: "#141414", color: "#d0d1d5", }}>
-                        <table className='w-full text-xl'>
-                            <thead>
-                                <tr className='border-b-4 border-slate-600 text-left pl-2'>
-                                    <th className='pl-2'>No.</th>
-                                    <th className='pl-2'>Title</th>
-                                    <th className='pl-2'>Cost</th>
-                                    <th className='pl-2'>Emp</th>
-                                    <th className='pl-2'>Customer</th>
-                                    <th className='pl-2'>Operations</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {workOrders.map((wo, index) => (
-                                    <tr key={wo._id} className='h-20 border-b border-slate-700'>
-                                        <td className='pl-2'>
-                                            {index + 1}
-                                        </td>
-                                        <td className='pl-2'>
-                                            {wo.title}
-                                        </td>
-                                        <td className='pl-2'>
-                                            {wo.cost}
-                                        </td>
-                                        <td className='pl-2'>
-                                            {wo.assignedEmp}
-                                        </td>
-                                        <td className='pl-2'>
-                                            {wo.customerID}
-                                        </td>
-                                        <td className='flex justify-evenly items-center  h-20'>
-
-                                            <Link to={`edit/${wo._id}`} className='link '>
-                                                <EditIcon />
-                                            </Link>
-                                            <Link to={`details/${wo._id}`} className='link'>
-                                                <InfoOutlinedIcon />
-                                            </Link>
-                                            <Link to={`delete/${wo._id}`} className='link'>
-                                                <DeleteOutlineIcon />
-                                            </Link>
-
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </Paper> */}
                 </Box>
             )}
         </div>
