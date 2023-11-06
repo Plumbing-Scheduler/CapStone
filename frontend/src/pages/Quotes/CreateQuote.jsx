ab42343359cbaa1f3e35b01d751384c77ae4e99f
christian <christian.langcuyan@hotmail.com>
2023-11-05 18:48:59 -0700

    changed styling for save and add button

--- frontend/src/pages/Quotes/CreateQuote.jsx
+++ frontend/src/pages/Quotes/CreateQuote.jsx
@@ -1,11 +1,14 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, TextField, Typography, Button, useTheme } from "@mui/material";
 import Header from "../../components/Header";
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
+import { tokens } from "../../theme";
 
 
 const Quotes = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [phone, setPhone] = useState('');
@@ -15,6 +18,7 @@ const Quotes = () => {
     const [busName, setBusName] = useState('');
     const [email, setEmail] = useState('');
     const navigate = useNavigate();
+
 
     const newQuote = {
         firstName,
@@ -44,123 +48,136 @@ const Quotes = () => {
         <Box >
             <Header title="QUOTE REQUESTS" subtitle="Add New Quote" />
 
-            <Typography
-                //display="flex"
-                variant="h4"
-                //justifyContent="space-between"
-                sx={{
-                    m: "10px auto",
-                    width: '80%',
-                    textAlign: 'center'
-                }}>
-            </Typography>
-            <Box
-                display="grid"
-                gap="30px"
-                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-                sx={{
-                    gridColumn: "span 4",
-                    margin: "auto",
-                    padding: "30px",
-                    width: '75%'
-                }} >
-                <TextField
-                    fullWidth
-                    type="text"
-                    variant='filled'
-                    label="First Name"
-                    name="firstName"
-                    id="firstName"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    sx={{ gridColumn: "span 2" }}
-                />
-                <TextField
-                    fullWidth
-                    type="text"
-                    variant='filled'
-                    label="Last Name"
-                    name="lastName"
-                    id="lastName"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    sx={{ gridColumn: "span 2" }}
-                />
-                <TextField
-                    fullWidth
-                    type="text"
-                    variant='filled'
-                    label="Phone #"
-                    name="phone"
-                    id="phone"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    sx={{ gridColumn: "span 2" }}
-                />
-                <TextField
-                    fullWidth
-                    type="text"
-                    variant='filled'
-                    label="Email"
-                    name="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    sx={{ gridColumn: "span 2" }}
-                />
+            <Box>
+                <Typography
+                    //display="flex"
+                    variant="h4"
+                    //justifyContent="space-between"
+                    sx={{
+                        m: "10px auto",
+                        width: '80%',
+                        textAlign: 'center'
+                    }}>
+                </Typography>
+                <Box
+                    display="grid"
+                    gap="30px"
+                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                    sx={{
+                        gridColumn: "span 4",
+                        margin: "auto",
+                        padding: "30px",
+                        width: '75%'
+                    }} >
+                    <TextField
+                        fullWidth
+                        type="text"
+                        variant='filled'
+                        label="First Name"
+                        name="firstName"
+                        id="firstName"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        sx={{ gridColumn: "span 2" }}
+                    />
+                    <TextField
+                        fullWidth
+                        type="text"
+                        variant='filled'
+                        label="Last Name"
+                        name="lastName"
+                        id="lastName"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        sx={{ gridColumn: "span 2" }}
+                    />
+                    <TextField
+                        fullWidth
+                        type="text"
+                        variant='filled'
+                        label="Phone #"
+                        name="phone"
+                        id="phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        sx={{ gridColumn: "span 2" }}
+                    />
+                    <TextField
+                        fullWidth
+                        type="text"
+                        variant='filled'
+                        label="Email"
+                        name="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        sx={{ gridColumn: "span 2" }}
+                    />
 
-                <TextField
-                    fullWidth
-                    type="text"
-                    variant="filled"
-                    label="Buisness Name"
-                    name="businessname"
-                    id="businessname"
-                    value={busName}
-                    onChange={(e) => setBusName(e.target.value)}
-                    sx={{ gridColumn: "span 2" }}
-                />
-                <TextField
-                    fullWidth
-                    type="text"
-                    variant="filled"
-                    label="Address"
-                    name="address"
-                    id="address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    sx={{ gridColumn: "span 2" }}
-                />
-                <TextField
-                    fullWidth
-                    type="number"
-                    variant='filled'
-                    label="Cost"
-                    name="cost"
-                    id="cost"
-                    value={cost}
-                    onChange={(e) => setCost(e.target.value)}
-                    sx={{ gridColumn: "span 2" }}
-                />
-                <TextField
-                    fullWidth
-                    multiline
-                    variant="filled"
-                    label="Description"
-                    required
-                    cols="30"
-                    rows="4"
-                    name="description"
-                    id="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    sx={{ gridColumn: "span 2" }}
-                />
-                <button onClick={handleSave} className='bg-gray-500 w-1/2 h-12 rounded-sm'>
-                    Save and Add
-                </button>
+                    <TextField
+                        fullWidth
+                        type="text"
+                        variant="filled"
+                        label="Buisness Name"
+                        name="businessname"
+                        id="businessname"
+                        value={busName}
+                        onChange={(e) => setBusName(e.target.value)}
+                        sx={{ gridColumn: "span 2" }}
+                    />
+                    <TextField
+                        fullWidth
+                        type="text"
+                        variant="filled"
+                        label="Address"
+                        name="address"
+                        id="address"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        sx={{ gridColumn: "span 2" }}
+                    />
+                    <TextField
+                        fullWidth
+                        type="number"
+                        variant='filled'
+                        label="Cost"
+                        name="cost"
+                        id="cost"
+                        value={cost}
+                        onChange={(e) => setCost(e.target.value)}
+                        sx={{ gridColumn: "span 2" }}
+                    />
+                    <TextField
+                        fullWidth
+                        multiline
+                        variant="filled"
+                        label="Description"
+                        required
+                        cols="30"
+                        rows="4"
+                        name="description"
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        sx={{ gridColumn: "span 2" }}
+
+                    />
+                </Box>
+                <div className="flex justify-end mr-60">
+                    <Button
+                        onClick={handleSave}
+                        sx={{
+                            backgroundColor: colors.redAccent[500],
+                            fontWeight: 'bold',
+                            fontSize: '13px',
+                            width: '15%',
+                            borderRadius: '3px'
+                        }}
+                    >
+                        Save and Add
+                    </Button>
+                </div>
             </Box>
-
         </Box>
     )
 }
