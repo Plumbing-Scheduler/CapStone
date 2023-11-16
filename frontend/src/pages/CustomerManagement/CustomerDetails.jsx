af9279a7480988d8986d53ffec63ad1fd0ee8a60
Jeremy <jeremy.andrei@edu.sait.ca>
2023-11-16 13:55:56 -0700

    added styling

--- frontend/src/pages/CustomerManagement/CustomerDetails.jsx
+++ frontend/src/pages/CustomerManagement/CustomerDetails.jsx
@@ -1,7 +1,7 @@
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Paper, Divider } from '@mui/material';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Spinner from 'react-bootstrap/Spinner';
 import dayjs from 'dayjs';
 import Header from '../../components/Header';
@@ -9,78 +9,65 @@ import { DeleteButton } from '../../components/global/DeleteButton';
 import { EditButton } from '../../components/global/EditButton';
 
 const CustomerDetails = () => {
-    const { id } = useParams();
-    const [customer, setCustomer] = useState('');
-    const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const [customer, setCustomer] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setLoading(true);
-        axios
-            .get(`http://localhost:3500/customer/${id}`)
-            .then((responce) => {
-                setCustomer(responce.data);
-                setLoading(false);
-                console.log(customer.startDate);
-            })
-            .catch((error) => {
-                console.log(error);
-                setLoading(false);
-            })
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:3500/customer/${id}`)
+      .then((response) => {
+        setCustomer(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, [id]);
 
-    return (
-        <Box>
-            <Box>
-                <Header title={"CUSTOMER"} subtitle={"DETAILS"} />
+  return (
+    <Box>
+      <Header title={'CUSTOMER'} subtitle={'DETAILS'} />
+      {loading ? (
+        <div className="w-5 m-auto h-5 pt-11 text-center">
+          <Spinner />
+        </div>
+      ) : (
+        <Box m={4}>
+          <div className="flex justify-end space-x-3">
+            <EditButton />
+            <DeleteButton />
+          </div>
+          <Paper elevation={3} sx={{ p: 3, mt: 3 }}>
+            <Typography variant="h4" sx={{ paddingBottom: '1rem' }}>
+              Customer Information
+            </Typography>
+            <Divider />
+            <Box mt={2}>
+              <Typography variant="body1">
+                <strong>Name:</strong> {customer.firstName} {customer.lastName}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Phone:</strong> {customer.phone}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Email:</strong> {customer.email}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Address:</strong> {`${customer.address.street}, ${customer.address.postalCode} ${customer.address.city}, ${customer.address.province}`}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Business Name:</strong> {customer.busName}
+              </Typography>
+              {/* Add more fields as needed */}
             </Box>
-            {loading ? (<div className='w-5 m-auto h-5 pt-11 text-center'><Spinner /></div>) : (
-
-                <div>
-                    <div className='flex justify-end m-4 space-x-3'>
-                        <EditButton />
-                        <DeleteButton />
-                    </div>
-                    <Box
-                        display="grid"
-                        gap="30px"
-                        gridTemplateColumns={"repeat(2, minmax(0, 1fr))"}
-                        sx={{
-                            gridColumn: "span 4",
-                            margin: "auto",
-                            width: '75%',
-                        }}
-                    >
-                        <Box>
-                            <Typography
-                                variant="h2"
-                                sx={{
-                                    width: '75%',
-                                    paddingBottom: '10px'
-                                }}>
-                                Customer Information
-                            </Typography>
-                            <Box>
-                                <Typography variant='body1'>{customer.firstName} {customer.lastName}</Typography>
-                                <Typography variant='body1'>{customer.phone}</Typography>
-                                <Typography variant='body1'>{customer.email}</Typography>
-                                <Typography variant='body1'>{customer.address.street}</Typography>
-                                <Typography variant='body1'>{customer.address.postalCode}</Typography>
-                                <Typography variant='body1'>{customer.address.city} {customer.address.province}</Typography>
-                                <Typography variant='body1'>{customer.busName}</Typography>
-                            </Box>
-                        </Box>
-
-                    
-
-                   
-                    </Box>
-                </div>
-            )}
+          </Paper>
         </Box>
+      )}
+    </Box>
+  );
+};
 
-
-    )
-}
-
-
-export default CustomerDetails
+export default CustomerDetails;
