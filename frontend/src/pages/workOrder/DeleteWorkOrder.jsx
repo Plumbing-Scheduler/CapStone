f8625eaf6420551e3b601e4422b094aa6740c171
Jeremy <jeremy.andrei@edu.sait.ca>
2023-11-16 20:32:09 -0700

    changed delete button and go back to proper titles and colours

--- frontend/src/pages/workOrder/DeleteWorkOrder.jsx
+++ frontend/src/pages/workOrder/DeleteWorkOrder.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, useTheme, Typography} from '@mui/material';
+import { Box, Button, useTheme, Typography } from '@mui/material';
 import { useParams, useNavigate, Link } from 'react-router-dom';
-import { tokens } from "../../theme";
+import { tokens } from '../../theme';
 import axios from 'axios';
 import Spinner from 'react-bootstrap/Spinner';
 import Header from '../../components/Header';
@@ -19,84 +19,94 @@ export const DeleteWorkOrder = () => {
     axios
       .get(`http://localhost:3500/workorders/${id}`)
       .then((response) => {
-        setWorkOrder(response.data)
+        setWorkOrder(response.data);
         setLoading(false);
         console.log(workOrder);
-      }
-      )
+      })
       .catch((error) => {
         setLoading(false);
-        console.log(error)
+        console.log(error);
       });
   }, [id]);
 
   const handleDeleteWorkOrder = () => {
     axios
       .delete(`http://localhost:3500/workorders/${id}`)
-      .then(
-        navigate('/workorder')
-      )
-      .catch((error) =>
-        console.log(error)
-      );
+      .then(navigate('/workorder'))
+      .catch((error) => console.log(error));
   };
 
-  return (<div>
-    <Header title={"WORK ORDER"} subtitle={"DELETE WORK ORDER"} />
-    {loading ? (<div className='w-5 m-auto h-5 pt-11 text-center'><Spinner /></div>) : (
-      <Box m="30px">
-        <Box sx={{ width: "90%", margin: "auto", boxShadow: '4', border: 'solid', borderWidth: "2px", borderRadius: '5px' }}>
-          <Typography variant="h2" fontWeight="bold" sx={{ mb: "5%", mt: '2%', textAlign: "center" }}>Delete Work Order?</Typography>
-          <div className='m-10'>
-            <div className='text-xl pl-2 pb-4'>
-              <span className='font-light'>Title: </span>
-              <span className='font-bold'>{workOrder.title}</span>
+  return (
+    <div>
+      <Header title={'WORK ORDER'} subtitle={'DELETE WORK ORDER'} />
+      {loading ? (
+        <div className="w-5 m-auto h-5 pt-11 text-center">
+          <Spinner />
+        </div>
+      ) : (
+        <Box m="30px">
+          <Box sx={{ width: '90%', margin: 'auto', boxShadow: '4', border: 'solid', borderWidth: '2px', borderRadius: '5px' }}>
+            <Typography variant="h2" fontWeight="bold" sx={{ mb: '5%', mt: '2%', textAlign: 'center' }}>
+              Delete Work Order?
+            </Typography>
+            <div className="m-10">
+              <div className="text-xl pl-2 pb-4">
+                <span className="font-light">Title: </span>
+                <span className="font-bold">{workOrder.title}</span>
+              </div>
+              <div className="text-xl pl-2 pb-4">
+                <span className="font-light">Description: </span>
+                <span className="font-bold">{workOrder.s_description}</span>
+              </div>
+              <div className="text-xl pl-2 pb-4">
+                <span className="font-light">Start Date: </span>
+                <span className="font-bold">{workOrder.s_startDate}</span>
+              </div>
+              <div className="text-xl pl-2 pb-4">
+                <span className="font-light">End Date: </span>
+                <span className="font-bold">{workOrder.endDate}</span>
+              </div>
+              <div className="text-xl pl-2 pb-4">
+                <span className="font-light">Cost: </span>
+                <span className="font-bold">${workOrder.s_cost}</span>
+              </div>
+              <div className="text-xl pl-2 pb-4">
+                <span className="font-light">Address: </span>
+                <span className="font-bold">{workOrder.address}</span>
+              </div>
+              <div className="text-xl pl-2 pb-4">
+                <span className="font-light">Business name: </span>
+                <span className="font-bold">{workOrder.busName}</span>
+              </div>
             </div>
-            <div className='text-xl pl-2 pb-4'>
-              <span className='font-light'>Description: </span>
-              <span className='font-bold'>{workOrder.s_description}</span>
-            </div>
-            <div className='text-xl pl-2 pb-4'>
-              <span className='font-light'>Start Date: </span>
-              <span className='font-bold'>{workOrder.s_startDate}</span>
-            </div>
-            <div className='text-xl pl-2 pb-4'>
-              <span className='font-light'>End Date: </span>
-              <span className='font-bold'>{workOrder.endDate}</span>
-            </div>
-            <div className='text-xl pl-2 pb-4'>
-              <span className='font-light'>Cost: </span>
-              <span className='font-bold'>${workOrder.s_cost}</span>
-            </div>
-            <div className='text-xl pl-2 pb-4'>
-              <span className='font-light'>Address: </span>
-              <span className='font-bold'>{workOrder.address}</span>
-            </div>
-            <div className='text-xl pl-2 pb-4'>
-              <span className='font-light'>Business name: </span>
-              <span className='font-bold'>{workOrder.busName}</span>
-            </div>
-          </div>
+          </Box>
+          <Box display="flex" justifyContent="space-between" sx={{ width: '20%', margin: 'auto', pt: '2%' }}>
+            <Link to={'/workorder'}>
+              <Button
+                sx={{
+                  backgroundColor: 'grey.500',
+                  fontWeight: 'bold',
+                  fontSize: '13px',
+                }}
+              >
+                Cancel
+              </Button>
+            </Link>
+            <Button
+              sx={{
+                backgroundColor: colors.redButton,
+                fontWeight: 'bold',
+                fontSize: '13px',
+              }}
+              onClick={handleDeleteWorkOrder}
+            >
+              Delete
+            </Button>
+          </Box>
         </Box>
-        <Box display="flex" justifyContent="space-between" sx={{ width: '20%', margin: 'auto', pt: '2%' }}>
-          <Link to={'/workorder'}>
-            <Button sx={{
-              backgroundColor: colors.greenButton,
-              fontWeight: 'bold',
-              fontSize: '13px',
-            }}
-            >No, Go Back</Button>
-          </Link>
-          <Button sx={{
-            backgroundColor: colors.redButton,
-            fontWeight: 'bold',
-            fontSize: '13px',
-          }} onClick={handleDeleteWorkOrder} >Yes, Im Sure</Button>
-        </Box>
-      </Box>
-    )}
-  </div >
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default DeleteWorkOrder
+export default DeleteWorkOrder;
