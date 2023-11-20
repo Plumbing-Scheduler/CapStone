d8bb34235ef56c70b58d32e6e09aa645a72f62f5
IsaacM1210 <1210Mhamed@gmail.com>
2023-11-19 21:44:03 -0700

    reports filter functioning

--- frontend/src/pages/Reports/ServiceReports.jsx
+++ frontend/src/pages/Reports/ServiceReports.jsx
@@ -18,11 +18,43 @@ const ServiceReports = () => {
     const colors = tokens(theme.palette.mode);
     const { filter } = useParams();
     const filterObj = JSON.parse(filter);
-    let filterWO;
+    const [filterWO, setFilterWO] = useState([{}]);
 
     const [employees, setEmployees] = useState([]);
     const [customers, setCustomers] = useState([]);
     const [workOrders, setWorkOrders] = useState([]);
+
+    const filtering = (wo) => {
+        if(filterObj.assignedEmp != ""){
+            if(wo.assignedEmp != filterObj.assignedEmp){
+                return false
+            }
+        }
+        if(filterObj.serviceType != ""){
+            if(wo.title != filterObj.serviceType){
+                return false
+            }
+        }
+        if(filterObj.paymentType != ""){
+            if(wo.paymentType != filterObj.paymentType){
+                return false
+            }
+        }
+        if(filterObj.customer != ""){
+            if(wo.customerID != filterObj.customer){
+                return false
+            }
+        }
+        if(filterObj.busName != ""){
+            if(wo.busName != filterObj.busName){
+                return false
+            }
+        }
+        if(wo.startDate < filterObj.startDate && wo.endDate > filterObj.startDate){
+                return false
+        }
+        return true
+    }
 
     useEffect(() => {
         axiosInstance.get('/workorders')
@@ -44,17 +76,32 @@ const ServiceReports = () => {
     }, []);
 
     useEffect(() => {
-        filterWO = workOrders.filter((wo) => (
-            wo.paymentType == filterObj.paymentType
-        ))
+        setFilterWO(workOrders.filter(filtering));
         console.log(filterWO);
     },[workOrders])
 
+    const getEmployee = (empId) => {
+        for (let i = 0; employees.length > i; i++) {
+            if (employees[i]._id === empId) {
+                return employees[i].firstName + ' ' + employees[i].lastName
+            }
+        }
+    }
+
+    const getCustomer = (custId) => {
+        for (let i = 0; customers.length > i; i++) {
+            if (customers[i]._id === custId) {
+                return customers[i].firstName + ' ' + customers[i].lastName
+            }
+        }
+    }
+
     return (
-        <Box>
+        <Box> 
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Header title="Reports" subtitle="" />
             </Box>
+            {filterWO.map((rp) => (
             <Box m={3}>
                 <Box mb={2}>
                     <Paper elevation={3} sx={{ backgroundColor: colors.primary[400], p: 2 }}>
@@ -70,7 +117,6 @@ const ServiceReports = () => {
                         <TableHead>
                             <TableRow>
                                 <TableCell>Service Type</TableCell>
-                                <TableCell>Commercial/Residential</TableCell>
                                 <TableCell>Employee</TableCell>
                                 <TableCell>Completion Date</TableCell>
                                 <TableCell>Payment Type</TableCell>
@@ -83,21 +129,19 @@ const ServiceReports = () => {
                             // key={row.name}
                             // sx = {{ '&: last-child td, &: last-child th' : {border:0}}}
                             >
-                                {/* <TableCell component="th" scope="row"></TableCell> */}
-                                <TableCell >
-                                    {/* {row.name} */}
-                                </TableCell>
-                                <TableCell align="right">{ }</TableCell>
-                                <TableCell align="right">{ }</TableCell>
-                                <TableCell align="right">{ }</TableCell>
-                                <TableCell align="right">{ }</TableCell>
+                                <TableCell component="th" scope="row">{rp.title }</TableCell>
+                                <TableCell align="right">{getEmployee(rp.assignedEmp)}</TableCell>
+                                <TableCell align="right">{rp.endDate }</TableCell>
+                                <TableCell align="right">{rp.paymentType }</TableCell>
+                                <TableCell align="right">{rp.cost}</TableCell>
+ 
                             </TableRow>
 
                         </TableBody>
                     </Table>
                 </TableContainer>
-
-            </Box>
+                </Box>
+            ))}
         </Box>
     )
 }
