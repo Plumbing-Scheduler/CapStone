d2f259e1df7f2178fde818d96dfb8786b247ae85
Jeremy <Jeremy.Andrei@edu.sait.ca>
2023-11-20 14:04:32 -0700

    Fixed buttons and reverted back to original edit and delete

--- frontend/src/components/global/DeleteButton.jsx
+++ frontend/src/components/global/DeleteButton.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, useTheme, IconButton } from "@mui/material";
+import { Box, useTheme, Button } from "@mui/material";
 import { tokens } from "../../theme";
-import DeleteIcon from '@mui/icons-material/Delete';
 
 export const DeleteButton = () => {
   const theme = useTheme();
@@ -11,12 +10,12 @@ export const DeleteButton = () => {
   return (
     <Box>
       <Link to='..' relative='path' className='link'>
-        <IconButton aria-label="delete" color="error" edge="start" sx={{ borderRadius: '50%' }}>
-          <DeleteIcon />
-        </IconButton>
+        <Box display="flex" justifyContent="space-between" backgroundColor={colors.redAccent[400]} borderRadius="3px" >
+          <Button variant="text">
+            Delete
+          </Button>
+        </Box>
       </Link>
     </Box>
   );
 }
-
-export default DeleteButton;
