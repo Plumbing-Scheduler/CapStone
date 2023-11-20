9bef4a436d7b3d132b62d1f5ad64f4febfb9259a
IsaacM1210 <1210Mhamed@gmail.com>
2023-11-20 10:46:04 -0700

    adjusted service reports table

--- frontend/src/pages/Reports/ServiceReports.jsx
+++ frontend/src/pages/Reports/ServiceReports.jsx
@@ -11,7 +11,9 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useParams } from 'react-router-dom';
 import axiosInstance from '../../axiosInstance';
-
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import Spinner from 'react-bootstrap/esm/Spinner';
 
 const ServiceReports = () => {
     const theme = useTheme();
@@ -19,68 +21,73 @@ const ServiceReports = () => {
     const { filter } = useParams();
     const filterObj = JSON.parse(filter);
     const [filterWO, setFilterWO] = useState([{}]);
-
+    const [loading, setLoading] = useState(true);
     const [employees, setEmployees] = useState([]);
     const [customers, setCustomers] = useState([]);
     const [workOrders, setWorkOrders] = useState([]);
+    dayjs.extend(localizedFormat);
 
     const filtering = (wo) => {
-        if(filterObj.assignedEmp != ""){
-            if(wo.assignedEmp != filterObj.assignedEmp){
+        if (wo.serviceStatus != "Completed") {
+            return false
+        }
+        if (filterObj.assignedEmp != "") {
+            if (wo.assignedEmp != filterObj.assignedEmp) {
                 return false
             }
         }
-        if(filterObj.serviceType != ""){
-            if(wo.title != filterObj.serviceType){
+        if (filterObj.serviceType != "") {
+            if (wo.title != filterObj.serviceType) {
                 return false
             }
         }
-        if(filterObj.paymentType != ""){
-            if(wo.paymentType != filterObj.paymentType){
+        if (filterObj.paymentType != "") {
+            if (wo.paymentType != filterObj.paymentType) {
                 return false
             }
         }
-        if(filterObj.customer != ""){
-            if(wo.customerID != filterObj.customer){
+        if (filterObj.customer != "") {
+            if (wo.customerID != filterObj.customer) {
                 return false
             }
         }
-        if(filterObj.busName != ""){
-            if(wo.busName != filterObj.busName){
+        if (filterObj.busName != "") {
+            if (wo.busName != filterObj.busName) {
                 return false
             }
         }
-        if(wo.startDate < filterObj.startDate && wo.endDate > filterObj.startDate){
-                return false
+        if (!dayjs(wo.startDate).isBetween(filterObj.startDate, filterObj.endDate)) {
+            return false
         }
         return true
     }
 
     useEffect(() => {
         axiosInstance.get('/workorders')
-        .then((responce) => {
-            setWorkOrders(responce.data.data);
-            axiosInstance.get('/employees')
-                .then((responce) => {
-                    setEmployees(responce.data.data);
-                })
-            axiosInstance.get('/customer')
-                .then((responce) => {
-                    setCustomers(responce.data.data);
-                })
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+            .then((responce) => {
+                setWorkOrders(responce.data.data);
+                axiosInstance.get('/employees')
+                    .then((responce) => {
+                        setEmployees(responce.data.data);
+                    })
+                axiosInstance.get('/customer')
+                    .then((responce) => {
+                        setCustomers(responce.data.data);
+                    })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
     }, []);
 
     useEffect(() => {
         setFilterWO(workOrders.filter(filtering));
-        console.log(filterWO);
-    },[workOrders])
+        console.log(filterWO[0]);
+        
+    }, [workOrders])
 
-    const getEmployee = (empId) => {
+    const getEmployeeName = (empId) => {
         for (let i = 0; employees.length > i; i++) {
             if (employees[i]._id === empId) {
                 return employees[i].firstName + ' ' + employees[i].lastName
@@ -91,55 +98,52 @@ const ServiceReports = () => {
     const getCustomer = (custId) => {
         for (let i = 0; customers.length > i; i++) {
             if (customers[i]._id === custId) {
-                return customers[i].firstName + ' ' + customers[i].lastName
+                return customers[i]
             }
         }
     }
 
     return (
-        <Box> 
+        <Box>
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Header title="Reports" subtitle="" />
             </Box>
-            {filterWO.map((rp) => (
-            <Box m={3}>
-                <Box mb={2}>
-                    <Paper elevation={3} sx={{ backgroundColor: colors.primary[400], p: 2 }}>
-                        <Typography variant="h6" ><b>Customer ID: </b></Typography>
-                        <Typography variant="h6" ><b>CustomerName: </b></Typography>
-                        <Typography variant="h6" ><b>Phone Number: </b></Typography>
-                        <Typography variant="h6" ><b>Address: </b></Typography>
-                        <Typography variant="h6" ><b>Email: </b></Typography>
-                    </Paper>
-                </Box>
-                <TableContainer component={Paper} sx={{ background: colors.primary[400] }} >
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Service Type</TableCell>
-                                <TableCell>Employee</TableCell>
-                                <TableCell>Completion Date</TableCell>
-                                <TableCell>Payment Type</TableCell>
-                                <TableCell>Cost</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {/* {rows.map((row) => ())} */}
-                            <TableRow
-                            // key={row.name}
-                            // sx = {{ '&: last-child td, &: last-child th' : {border:0}}}
-                            >
-                                <TableCell component="th" scope="row">{rp.title }</TableCell>
-                                <TableCell align="right">{getEmployee(rp.assignedEmp)}</TableCell>
-                                <TableCell align="right">{rp.endDate }</TableCell>
-                                <TableCell align="right">{rp.paymentType }</TableCell>
-                                <TableCell align="right">{rp.cost}</TableCell>
- 
-                            </TableRow>
-
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+            {filterWO.map((wo) => (
+                <Box m={3}>
+                    <Box mb={2}>
+                        <Paper elevation={3} sx={{ backgroundColor: colors.primary[400], p: 2 }}>
+                            {/**Bug here, when page is rendered exception is thrown because technically "filterWO" is undefined*/}
+                             <Typography variant="h6" ><b>Customer ID: </b>{wo.customerID}</Typography>
+                            <Typography variant="h6" ><b>CustomerName: </b>{getCustomer(wo.customerID).firstName + ' ' + getCustomer(wo.customerID).lastName}</Typography>
+                            <Typography variant="h6" ><b>Phone Number: </b>{getCustomer(wo.customerID).phone}</Typography>
+                            <Typography variant="h6" ><b>Address: </b>{getCustomer(wo.customerID).address.street}</Typography>
+                            <Typography variant="h6" ><b>Email: </b>{getCustomer(wo.customerID).email}</Typography>
+                        </Paper>
+                    </Box>
+                    <TableContainer component={Paper} sx={{ background: colors.primary[400] }} >
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell><b>Service Type</b></TableCell>
+                                    <TableCell><b>Employee</b></TableCell>
+                                    <TableCell><b>Completion Date</b></TableCell>
+                                    <TableCell><b>Payment Type</b></TableCell>
+                                    <TableCell><b>Cost</b></TableCell>
+                                    <TableCell><b>Status</b></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell align="left">{wo.title}</TableCell>
+                                    <TableCell align="left">{getEmployeeName(wo.assignedEmp)}</TableCell>
+                                    <TableCell align="left">{dayjs(wo.endDate).format("ll")}</TableCell>
+                                    <TableCell align="left">{wo.paymentType}</TableCell>
+                                    <TableCell align="left">${wo.cost}</TableCell>
+                                    <TableCell align="left">{wo.serviceStatus}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Box>
             ))}
         </Box>
