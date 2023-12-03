6fc6511798081cbd097f12ab1737a8b6b69e2d75
IsaacM1210 <1210Mhamed@gmail.com>
2023-12-03 09:27:03 -0700

    made DB URI Hidden

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,6 @@ app.use(express.json());
 
 //Default Route
 app.get('/' ,(request, response) => {
-    //console.log(request);
     return response.status(234).send('Welcome to the beggining of the end!')
 });
 app.use('/login', auth)
@@ -58,11 +57,9 @@ app.use('/customer', Customer);
 app.use('/quote', Quote)
 app.use('/employees', Employee)
 app.use('/report', report)
-//Connection to MongoDB
-//Connection Process will need to be changed so than Mongo URL isnt in plain text!!!!!!
-//for Development Process mongodb+srv://Admin:hvHVFgTb1Ljxf4oC@plumbing-scheduler.rqelmif.mongodb.net/?retryWrites=true&w=majority
+
 mongoose
-    .connect('mongodb+srv://Admin:hvHVFgTb1Ljxf4oC@plumbing-scheduler.rqelmif.mongodb.net/?retryWrites=true&w=majority')
+    .connect(process.env.DATABASE_URI)
     .then(() => {
         console.log('App Connected to DataBase');
         app.listen(PORT, () => {
